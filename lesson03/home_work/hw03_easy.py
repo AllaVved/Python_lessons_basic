7128204033c8a092ef01945773a1809c827e9faf
AllaVved <46631193+AllaVved@users.noreply.github.com>
2019-01-22 16:19:43 +0300

    Домашнее задание № 3

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,7 @@
+# coding: utf-8
+
+__author__ = 'Введенская Алла'
+
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,7 +9,21 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+	
+# Пример 1
+
+    	number = number * (10 ** ndigits)
+	if float(number) - int(number) > 0.5:
+		number = number // 1 + 1
+	else:
+		number = number // 1
+	return number / (10 ** ndigits)
+	
+# Пример 2
+
+	number = number*(10**ndigits)+0.4
+	number = number//1
+	return number/(10**ndigits) 
 
 
 print(my_round(2.1234567, 5))
@@ -20,8 +38,17 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
+	
+	if (len(str(ticket_number))!= 6) or (type(ticket_number) is not int):
+		return f'Билет {ticket_number} номер некорректный '
+	number = str(ticket_number)
+	sumA = int(number[0]) + int(number[1]) + int(number[2])
+	sumB = int(number[3]) + int(number[4]) + int(number[5])
+	if sumA == sumB:
+		return f'Билет {ticket_number} счастливый' 
+	else:
+		return f'Билет {ticket_number} несчастливый'
+		
 
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -1,9 +1,26 @@
+# coding: utf-8
+
+__author__ = 'Введенская Алла'
+
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+
+	fib_list = []
+	a, b = 1, 1
+	while a <= m:
+		a, b = b, a + b
+		if n <= a <= m:
+			fib_list.append(a)
+	return fib_list
+
+n = int(input('Введите начальное число Фибоначчи: '))
+m = int(input('Введите конечное значение: '))
+
+print (f"Числа Фибоначчи в диапозоне от {n} до {m}: {fibonacci(n, m)}" )
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,16 +29,42 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
-
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+	n = len(origin_list)
+	
+	for j in range(0,n-1):
+		for i in range(0,n-j-1):
+			if origin_list[i] < origin_list[i+1]:
+				origin_list[i+1], origin_list[i] = origin_list[i], origin_list[i+1]
+	return origin_list[::-1]
+	
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+def filter_list(fucnt, param):
+    new_list = list()
+    for x in param:
+        if fucnt(x) == True:
+            new_list.append(x)
+        else:
+            continue
+    return new_list
+ 
+ 
+print((filter_list((lambda x: x > 10), param = [10, 57, 158, 259, 2, 98, 5, 46, 8, 9, 598])))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+def is_parallelogram(A1, A2, A3, A4):
+	
+	if (A3[0] - A2[0]) == (A4[0] - A1[0]) and \
+		(A2[1] - A1[1]) == (A3[1] - A4[1]):
+		return "Это вершины параллелограмма"
+	return "Это вершины не параллелограмма"
+	
+print (is_parallelogram((2, 2), (4, 5), (9, 5), (7, 2)))
+print (is_parallelogram((2, 2), (4, 5), (10, 5), (7, 2)))
